0493c2811a4546870668646b102e83da76a9313a
Marcus Caetano <caetanomarcusf@gmail.com>
2024-08-14 12:10:07 -0300

    imports de naive bayes

--- usoNaiveBayes.py
+++ usoNaiveBayes.py
@@ -1,1 +1,11 @@
-#ver pressão no 18:38
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+from yellowbrick.classifier import ConfusionMatrix
+
+base = pd.read_csv('Data/insurance.csv')
+
+base.drop(['Unnamed: 0'], axis=1, inplace=True)
+print(base.head())
